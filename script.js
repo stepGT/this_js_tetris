1a29a9d8220593e35fa6d8a67e0fb47bddf86458
Andrey Zuev <stepgt@mail.ru>
2023-06-20 14:50:09 +1000

    Added drawTetromino && Styled tetromino

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 import { Tetris } from './tetris.js';
+import { convertPositionToIndex } from './utils.js';
+
 const tetris = new Tetris();
+const cells = document.querySelectorAll('.grid>div');
+//
+const draw = () => {
+  cells.forEach((cell) => cell.removeAttribute('class'));
+  drawTetromino();
+};
+
+const drawTetromino = () => {
+  const name = tetris.tetromino.name;
+  const tetrominoMatrixSize = tetris.tetromino.matrix.length;
+  for (let row = 0; row < tetrominoMatrixSize; row++) {
+    for (let column = 0; column < tetrominoMatrixSize; column++) {
+      if (!tetris.tetromino.matrix[row][column]) continue;
+      if (tetris.tetromino.row + row < 0) continue;
+      const cellIndex = convertPositionToIndex(
+        tetris.tetromino.row + row,
+        tetris.tetromino.column + column,
+      );
+      cells[cellIndex].classList.add(name);
+    }
+  }
+};
+
+draw();
--- tetris.js
+++ tetris.js
@@ -1,17 +1,35 @@
-import { PLAYFIELD_COLS, PLAYFIELD_ROWS } from './utils.js';
+import { PLAYFIELD_COLS, PLAYFIELD_ROWS, TETROMINO_NAMES, TETROMINOES, getRandomElement } from './utils.js';
 
 export class Tetris {
   constructor() {
     this.playfield;
+    this.tetromino;
     this.init();
   }
 
   init() {
     this.generatePlayfield();
+    this.generateTetromino();
   }
 
   generatePlayfield() {
     this.playfield = new Array(PLAYFIELD_ROWS).fill().map(() => new Array(PLAYFIELD_COLS).fill(0));
-    console.table(this.playfield);
+  }
+
+  generateTetromino() {
+    const name = getRandomElement(TETROMINO_NAMES);
+    const matrix = TETROMINOES[name];
+
+    const column = PLAYFIELD_COLS / 2 - Math.floor(matrix.length / 2);
+    const row = 3;
+
+    this.tetromino = {
+      name,
+      matrix,
+      row,
+      column,
+      ghostColumn: column,
+      ghostRow: row,
+    };
   }
 }
