0003e3a15f1df4fa1fed210384cb6d6e86551528
Andrey Zuev <stepgt@mail.ru>
2023-06-23 10:31:21 +1000

    Added isOutsideOfGameBoard && isCollides && isValid methods

--- tetris.js
+++ tetris.js
@@ -42,18 +42,56 @@ export class Tetris {
 
   moveTetrominoDown() {
     this.tetromino.row += 1;
+    if (!this.isValid()) {
+      this.tetromino.row -= 1;
+    }
   }
 
   moveTetrominoLeft() {
     this.tetromino.column -= 1;
+    if (!this.isValid()) {
+      this.tetromino.column += 1;
+    }
   }
 
   moveTetrominoRight() {
     this.tetromino.column += 1;
+    if (!this.isValid()) {
+      this.tetromino.column -= 1;
+    }
   }
 
   rotateTetromino() {
+    const oldMatrix = this.tetromino.matrix;
     const rotatedMatrix = rotateMatrix(this.tetromino.matrix);
     this.tetromino.matrix = rotatedMatrix;
+    if (!this.isValid()) {
+      this.tetromino.matrix = oldMatrix;
+    }
+  }
+
+  isOutsideOfGameBoard(row, column) {
+    return (
+      this.tetromino.column + column < 0 ||
+      this.tetromino.column + column >= PLAYFIELD_COLS ||
+      this.tetromino.row + row >= this.playfield.length
+    );
+  }
+
+  isCollides(row, column) {
+    return this.playfield[this.tetromino.row + row]?.[this.tetromino.column + column];
+  }
+
+  isValid() {
+    const matrixSize = this.tetromino.matrix.length;
+    //
+    for (let row = 0; row < matrixSize; row++) {
+      for (let column = 0; column < matrixSize; column++) {
+        if (!this.tetromino.matrix[row][column]) continue;
+        if (this.isOutsideOfGameBoard(row, column)) return false;
+        if (this.isCollides(row, column)) return false;
+      }
+    }
+    return true;
   }
 }
