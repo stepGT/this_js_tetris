3a8ceca570706d1ad9350a98fd7955935f4ee53c
Andrey Zuev <stepgt@mail.ru>
2023-06-23 13:54:56 +1000

    Added delete filled rows

--- tetris.js
+++ tetris.js
@@ -106,6 +106,36 @@ export class Tetris {
           this.tetromino.name;
       }
     }
+    this.processFilledRows();
     this.generateTetromino();
+  }
+
+  processFilledRows() {
+    const filledLines = this.findFilledRows();
+    this.removeFilledRows(filledLines);
+  }
+
+  findFilledRows() {
+    const filledRows = [];
+    //
+    for (let row = 0; row < PLAYFIELD_ROWS; row++) {
+      if (this.playfield[row].every((cell) => Boolean(cell))) {
+        filledRows.push(row);
+      }
+    }
+    return filledRows;
+  }
+
+  removeFilledRows(filledRows) {
+    filledRows.forEach((row) => {
+      this.dropRowsAbove(row);
+    });
+  }
+
+  dropRowsAbove(rowToDelete) {
+    for (let row = rowToDelete; row > 0; row--) {
+      this.playfield[row] = this.playfield[row - 1];
+    }
+    this.playfield[0] = new Array(PLAYFIELD_COLS).fill(0);
   }
 }
